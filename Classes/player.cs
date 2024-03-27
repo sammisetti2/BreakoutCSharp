ec98f84adfc54acf361b198bceb6dabf43406648
Sundeep Kumar Ammisetti <sammisetti2@gmail.com>
2024-03-26 19:18:34 -0500

    Created Player and modified walls
    - Created Player and added basic movement
    - Moved the walls to look better

---
+++ Classes/player.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody2D
+{
+    [Export]
+    public int speed = 500;
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        Vector2 direction = Vector2.Zero;
+
+        if(Input.IsActionPressed("move_left"))
+        {
+            direction  = Vector2.Left;
+        }
+
+        if(Input.IsActionPressed("move_right"))
+        {
+            direction = Vector2.Right;
+        }
+
+        Velocity = direction * speed;
+        MoveAndSlide();
+
+    }
+}
